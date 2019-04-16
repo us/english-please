c1247d51b9100977d9e95d1a2533fdbf5e2982f9
us <rahmetsaritekin@gmail.com>
2019-04-17 02:44:17 +0300

    Make-up scripts

--- dreate.py
+++ dreate.py
@@ -1,12 +1,12 @@
-
 from github import Github
-from six.moves import urllib
-import pprint
 import requests
 from langdetect import detect
 from bs4 import BeautifulSoup
 from markdown import markdown
 import re
+
+g = Github("")  # github auth token
+
 
 def markdown_to_text(markdown_string):
     """ Converts a markdown string to plaintext """
@@ -25,34 +25,35 @@ def markdown_to_text(markdown_string):
     return text
 
 
-
-g = Github("761a81080d3e4cb906bc95846c42bf1bd0730535")
-
-
-repo = g.get_repo("hemreari/easy-comment")
-# repo.create_issue(title="This is a new issue", body="This is the issue body")
-
-# a = urllib.request.urlopen('https://github-trending-api.now.sh/developers?language=python').read()
-response = requests.get('https://github-trending-api.now.sh/developers')
-response = response.json()
-#pprint.pprint(response[0]['repo']['description'])
-
-repo_name = response[4]['username'] + '/' + response[4]['repo']['name']
-print(repo_name)
-
-#repo = g.get_repo(repo_name)
-readme = repo.get_readme().decoded_content
-#readme = 'https://raw.githubusercontent.com/{}/master/README.md'.format(repo_name)
-readme = markdown_to_text(readme)
-md_language = detect(readme)
-
-f=open("issue_body.md", "r")
-
-if md_language == 'en':
-	issue = repo.create_issue(title="This is a new issue", body=f.read())
-	print(issue, 'Issue created.')
+def get_trending_as_json():
+    response = requests.get('https://github-trending-api.now.sh/developers')
+    response = response.json()
+    return response
 
 
-# print(readme)
-# print('=========')
-print(detect(readme))
+def check_repo(repo_name):
+    repo = g.get_repo(repo_name)
+    readme = repo.get_readme().decoded_content
+    readme = markdown_to_text(readme)
+    md_language = detect(readme)
+
+    f = open("issue_body.md", "r")
+
+    # if md_language == 'en':
+    #   #issue = repo.create_issue(title="This is a new issue", body=f.read())
+    #   print('Issue created.')
+    f.close()
+    print(detect(readme))
+
+
+def main():
+    print("python main function")
+
+    for repo in get_trending_as_json():
+        repo_name = repo['username'] + '/' + repo['repo']['name']
+        print(repo_name)
+        check_repo(repo_name)
+    print(check_repo('linlinjava/litemall'))
+
+if __name__ == '__main__':
+    main()
