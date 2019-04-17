94c3ae0e7d5eb9328e471688b6404ae284712c86
us <rahmetsaritekin@gmail.com>
2019-04-17 15:58:54 +0300

    Add functions comment.
    Add created_repo_issues.md file.

---
+++ created_repo_issues.md
@@ -0,0 +1,6 @@
+# Created Issues
+
+These are the issues created by this repo. 
+
+
+ - [us/db-prese](https://github.com/us/db-prese/issues/13)
--- dreate.py
+++ dreate.py
@@ -11,29 +11,25 @@ _issue_template = " - [{repo}](https://github.com/{repo}/issues/{issue_number})\
 
 
 def markdown_to_text(markdown_string):
-    """ Converts a markdown string to plaintext """
-
-    # md -> html -> text since BeautifulSoup can extract text cleanly
+    """ Cleaning md file to detect method """
     html = markdown(markdown_string)
-
-    # remove code snippets
     html = re.sub(r'<pre>(.*?)</pre>', ' ', html)
     html = re.sub(r'<code>(.*?)</code >', ' ', html)
 
-    # extract text
     soup = BeautifulSoup(html, "html.parser")
     text = ''.join(soup.findAll(text=True))
-
     return text
 
 
 def get_trending_as_json():
+    """ Get github trending as json from unofficial trending api. """
     response = requests.get('https://github-trending-api.now.sh/developers')
     response = response.json()
     return response
 
 
 def check_repo_language(repo_name):
+    """Get, clean and check repos README.md language"""
     repo = g.get_repo(repo_name)
     readme = repo.get_readme().decoded_content
     readme = markdown_to_text(readme)
@@ -44,24 +40,25 @@ def check_repo_language(repo_name):
 
 
 def creating_issue(repo, repo_language):
+    """ Create issue according to README.md language. """
     f = open("issue_body.md", "r", encoding="utf-8")
     created_repos_file = open("created_repo_issues.md", "r", encoding="utf-8")
     created_repos = created_repos_file.read()
     if repo_language == 'en' and repo.full_name not in created_repos:
-        issue = repo.create_issue(title="About sharing knowledge", body=f.read())
-        print('Issue created.')
+        issue = repo.create_issue(title="About sharing knowledge",
+                                  body=f.read())
+        print('Issue created in {}.'.format(repo.full_name))
         save_created_issues(repo.full_name, issue.number)
     f.close()
 
 
 def save_created_issues(repo, issue_number):
+    """ Write created issues to `created_repo_issues.md` """
     f = open("created_repo_issues.md", "a+")
     f.write(_issue_template.format(repo=repo, issue_number=issue_number))
 
 
 def main():
-    print("python main function")
-
     for repo in get_trending_as_json():
         repo_name = repo['username'] + '/' + repo['repo']['name']
         print(repo_name)
